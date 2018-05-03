2cb65617169804d0db820ddbb7c38e17d8b98752
peter92130 <410421215@ems.ndhu.edu.tw>
2018-05-03 21:28:11 +0800

    no message

--- 410421215.py
+++ 410421215.py
@@ -18,3 +18,29 @@ for y in range(H):
         data[y*400+x][1] =  key1.getpixel((x,y))
         data[y*400+x][2] =  key2.getpixel((x,y))
         E_ar[y*400+x]    =  E.getpixel((x,y))
+
+w = np.array([0,0,0])
+
+A = 0.00001
+
+maxlimit = 10
+w0 = 0
+w1 = 0
+w2 = 0
+
+epoch = 1
+while epoch < maxlimit and abs(w[0] - w0) > 0.00001 and abs(w[1] - w1) > 0.00001 and abs(w[2] - w2) > 0.00001:
+    
+    w0 = w[0]
+    w1 = w[1]
+    w2 = w[2]
+    
+    for i, x in enumerate(data): 
+            a = w[0] * x[0] + w[1] * x[1] + w[2] * x[2]
+            e = E_ar[i] - a
+            w = w + A * e * x
+
+    print(w[0], w[1], w[2])
+    epoch += 1
+
+
