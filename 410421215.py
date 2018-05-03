4d9f0616873176bd4f6896f7adad3f2c624a44e1
peter92130 <410421215@ems.ndhu.edu.tw>
2018-05-03 21:48:41 +0800

    no message

--- 410421215.py
+++ 410421215.py
@@ -2,28 +2,37 @@ from PIL import Image
 import numpy as np
 import scipy.misc
 
+#讀取圖檔
+
 key1    = Image.open("key1.png")
 key2    = Image.open("key2.png")
 I       = Image.open("I.png")
 E       = Image.open("E.png")
 Eprime  = Image.open("Eprime.png")
 
+#設置高寬
+
 W , H = key1.size
+
+#創建array
 
 data = np.zeros((120000,3),int)
 E_ar = np.zeros((120000),int)
 
+#讀入key1,key2,I至data
+
 for y in range(H):
     for x in range(W):
-        data[y*400+x][0] =  I.getpixel((x,y))
-        data[y*400+x][1] =  key1.getpixel((x,y))
-        data[y*400+x][2] =  key2.getpixel((x,y))
+        data[y*400+x][0] =  key1.getpixel((x,y))
+        data[y*400+x][1] =  key2.getpixel((x,y))
+        data[y*400+x][2] =  I.getpixel((x,y))
         E_ar[y*400+x]    =  E.getpixel((x,y))
 
 w = np.array([0,0,0])
 
-A = 0.00001
+#LMS
 
+A = 0.00001
 maxlimit = 10
 w0 = 1
 w1 = 1
@@ -44,12 +53,13 @@ while epoch < maxlimit and abs(w[0] - w0) > 0.00001 and abs(w[1] - w1) > 0.00001
     print(w[0], w[1], w[2])
     epoch += 1
 
+#輸出
 
 out = np.zeros((H,W),int)
 
 for y in range(H):
     for x in range(W):
-        out[y][x] = (Eprime.getpixel((x,y)) - w[1] *key1.getpixel((x,y)) -w[2] * key2.getpixel((x,y)))/w[0]
+        out[y][x] = (Eprime.getpixel((x,y)) - w[0] *key1.getpixel((x,y)) -w[1] * key2.getpixel((x,y)))/w[2]
         
         
     
